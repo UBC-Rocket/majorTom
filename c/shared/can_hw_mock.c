378d5f55fa4ec6c439285a2f306fa5ed96e242c9
Matt Peterson <mattpeterson145@gmail.com>
2017-10-29 14:19:33 -0700

    Added file can.c for hardware-independent CAN code. Also added error codes

--- c/shared/can_hw_mock.c
+++ c/shared/can_hw_mock.c
@@ -1,7 +1,8 @@
 /*
-Mock implementation of CAN bus library
+Mock implementation of CAN bus library's hardware-dependent portion
 */
 
+#include <general.h>
 #include <shared/can.h>
 
 //Mock implementations of functions from can.h
