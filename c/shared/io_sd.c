942847f4441df03df1a480bd9b8afb647c68f1ad
Rai McKenzie <rai.mckenziee@gmail.com>
2017-11-26 13:05:00 -0800

    Removed io function prefix. Added string log function.

--- c/shared/io_sd.c
+++ c/shared/io_sd.c
@@ -6,15 +6,29 @@ Mock implementation of IO SD card library
 #include <shared/init.h>
 
 /*
-
-
+@brief Mock implementation of writing doubles to a SD file log.
+@param Array of doubles
+@param Length of double array.
+@return status_t
 */
-status_t io_sdWrite(double[] output, int length) {
-	fprintf(file_sd, "[TIMESTAMP] "); //Add timestamp to print.
+status_t sdWriteDoublue(double[] output, int length) {
+	fprintf(file_sd, "[TIMESTAMP] ");
 	for (int i = 0; i < length; i++) {
 		fprintf(file_sd, "%d", output[i]);
 	}
 	fprintf(file_sd, "\n");
+
+	return STATUS_OK;
+}
+
+/*
+@brief Mock implementation of writing a string to a SD file log.
+@param String
+@return status_t
+*/
+status_t sdWriteString(char[] output) {
+	fprintf(file_sd, "[TIMESTAMP] %s\n", output);
+	return STATUS_OK;
 }
 
 //Add read function.
