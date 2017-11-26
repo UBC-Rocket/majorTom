411ccde62292bf05aef4c8a54bb73f76f8130e68
Rai McKenzie <rai.mckenziee@gmail.com>
2017-11-26 14:35:07 -0800

    Added timestamp to SD file logging.

--- c/shared/sd_hw_mock.c
+++ c/shared/sd_hw_mock.c
@@ -4,6 +4,7 @@ Mock implementation of IO SD card library
 
 #include <shared/sd.h>
 #include <shared/init.h>
+#include <shared/mock_utils.h>
 
 /*
 @brief Mock implementation of writing doubles to a SD file log.
@@ -12,7 +13,8 @@ Mock implementation of IO SD card library
 @return status_t
 */
 status_t sdWriteDouble(double output[], int length) {
-	fprintf(file_sd, "[TIMESTAMP] ");
+	time_t timestamp = getTimeSec();
+	fprintf(file_sd, "[%ld] ", timestamp);
 	for (int i = 0; i < length; i++) {
 		fprintf(file_sd, "%lf ", output[i]);
 	}
@@ -27,6 +29,7 @@ status_t sdWriteDouble(double output[], int length) {
 @return status_t
 */
 status_t sdWriteString(char output[]) {
-	fprintf(file_sd, "[TIMESTAMP] %s\n", output);
+	time_t timestamp = getTimeSec();
+	fprintf(file_sd, "[%ld] %s\n", timestamp, output);
 	return STATUS_OK;
 }
