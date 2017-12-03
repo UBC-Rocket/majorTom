3de4c0132c4bc22de16a20a3f805f70488820abe
cwaffles <cwaffles@users.noreply.github.com>
2017-12-03 12:39:19 -0800

    Revert "work in progress towards telem mocking"
    This reverts commit eb30bf7ae17b8dcbab4c395ed69949efe08b3d8d.

--- h/general.h
+++ h/general.h
@@ -10,8 +10,7 @@ typedef enum status_enum {
 	STATUS_OK        =  0,
 	STATUS_ERROR     = -1,
 	STATUS_BAD_PARAM = -2,
-	STATUS_CAN_FILTERED	= -3,
-	STATUS_NO_DATA   = -4
+	STATUS_CAN_FILTERED	= -3
 } status_t;
 
 #endif
